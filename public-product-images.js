3f2db297f17e45def7bfa52cc3ace5b6a930a2e1
ranchoflexivel-creator <ranchoflexivel@gmail.com>
2026-08-16 22:46:25 +0200

    Restore public sections without adding another page script

--- public-product-images.js
+++ public-product-images.js
@@ -1,4 +1,5 @@
 // As imagens dos produtos já são renderizadas pelo app.js a partir dos dados
 // carregados. Evitamos uma segunda consulta ao Supabase e um MutationObserver
 // permanente, que deixavam a página pesada.
+import "./public-content-fallback.js";
 import "./public-cart-ui-fix.js";
